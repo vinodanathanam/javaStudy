976d441addeb0bcf2f238c7a342f97950e7d173e
vinod <entejunk@gmail.com>
2015-10-19 18:44:05 +0100

    Initial checkin

---
+++ First/src/apples.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class apples {
+	static void testmethod(){
+		
+		tuna tobj = new tuna();
+		tobj.simplemsg();
+		
+	//	System.out.println("test method");
+	//	double tuna = 5.28;
+	//	System.out.println(tuna);
+		
+	/*	Scanner vinod = new Scanner(System.in);
+		
+		double fnum, snum, ans;
+		
+		System.out.println("Enter first number : ");
+		fnum = vinod.nextDouble();
+		
+		System.out.println("Enter second number : ");
+		snum = vinod.nextDouble();
+		
+		ans = fnum + snum;
+		
+		System.out.println(ans);
+		*/
+		
+	}
+	
+	public static void main(String args[]){
+	//	System.out.println("hello youtube");
+		testmethod();
+	}
+}
---
+++ First/src/tuna.java
@@ -0,0 +1,6 @@
+
+public class tuna {
+	public void simplemsg(){
+		System.out.println("This is my new class");
+	}
+}
